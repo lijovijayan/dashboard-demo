6aa5d29041ccbaf6bc0289b8761d07afb292d697
lijovijayan <lijovijayan00@gmail.com>
2022-06-18 19:42:53 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const ghpages = require("gh-pages");
+var fs = require("fs");
+
+const appPath = "../gather-dashboard/build/";
+const domain = "gather.lijovijayan.dev";
+
+// setting domain name for github pages to detect
+fs.writeFile(`${appPath}CNAME`, domain, function (err) {
+    console.log(err);
+});
+
+// setting up 404 page
+fs.writeFile(
+    `${appPath}404.html`,
+    `<meta http-equiv="refresh" content="0; url=https://${domain}" />`,
+    function (err) {
+        console.log(err);
+    }
+);
+
+ghpages.publish(appPath, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(`application is published at https://${domain}`);
+    }
+});
